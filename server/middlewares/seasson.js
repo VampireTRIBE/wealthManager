af6619e80a8dd93afe1b539a0d0fb31d367e9118
VampireTRIBE <amirsheikhvia@gmail.com>
2025-11-23 20:31:04 +0530

    update

--- server/middlewares/seasson.js
+++ server/middlewares/seasson.js
@@ -3,16 +3,14 @@ const MongoStore = require("connect-mongo");
 const log = require("../utills/logers/logger");
 
 function sessionConfig(app) {
-  store = MongoStore.create({
+  const store = MongoStore.create({
     mongoUrl: process.env.DB_URL,
-    crypto: {
-      secret: process.env.SESSION_SECRET,
-    },
+    crypto: { secret: process.env.SESSION_SECRET },
     touchAfter: 24 * 3600,
   });
 
-  store.on("error", () => {
-    log.error("MONGO SESSON STORE", err);
+  store.on("error", (err) => {
+    log.error("MONGO SESSION STORE ERROR:", err);
   });
 
   const sessionOptions = {
@@ -22,11 +20,12 @@ function sessionConfig(app) {
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      secure: true, 
-      sameSite: "none",
+      secure: process.env.NODE_ENV === "production", // Fix 🚀
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // Fix 🚀
       maxAge: 7 * 24 * 60 * 60 * 1000,
     },
   };
+
   app.use(session(sessionOptions));
 
   log.success("SESSION CONNECTED WITH MONGODB");
